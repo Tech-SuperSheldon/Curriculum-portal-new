7cb4e0e663ba4acf97366a07e6c3fcfaabe09469
Aditya Mishra <adityababaromio@gmail.com>
2025-11-02 14:14:05 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { useState } from "react";
-import LoginForm from "@/components/LoginForm";
+import LoginForm from "@/components/Viewer";
 
 export default function Home() {
   const [error, setError] = useState("");
 
   return (
     <main className="flex items-center justify-center h-screen bg-slate-900 text-white">
-      <LoginForm error={error} />
+      <Viewer/>
     </main>
   );
 }
